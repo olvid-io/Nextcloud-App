86fa8b15e82aa5720dac3bd39f3cdaf1ca6f3592
jmartel <jeremie.martel@olvid.io>
2026-05-20 11:36:57 +0200

    Page: simplified code, and now properly using Vue template

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -4,61 +4,26 @@ declare(strict_types=1);
 
 namespace OCA\Olvid\Controller;
 
-use Exception;
 use OCA\Olvid\AppInfo\Application;
-use OCA\Olvid\Utils\ServerConfigurationUtils;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\Attribute\FrontpageRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\TemplateResponse;
-use OCP\IAppConfig;
-use OCP\IRequest;
-use OCA\OIDCIdentityProvider\Db\ClientMapper as OidcClientMapper;
-use OCP\IURLGenerator;
-use Psr\Log\LoggerInterface;
 
 /**
  * @psalm-suppress UnusedClass
  */
 class PageController extends Controller {
-	private IAppConfig $appConfig;
-	private OidcClientMapper $oidcClientMapper;
-	private LoggerInterface $logger;
-	private IURLGenerator $urlGenerator;
-
-	public function __construct(
-		string $appName,
-		IRequest $request,
-		IAppConfig $appConfig,
-		OidcClientMapper $clientMapper,
-		LoggerInterface $logger,
-		IURLGenerator $urlGenerator
-	) {
-		parent::__construct($appName, $request);
-		$this->appConfig = $appConfig;
-		$this->oidcClientMapper = $clientMapper;
-		$this->logger = $logger;
-		$this->urlGenerator = $urlGenerator;
-
-	}
 	#[NoCSRFRequired]
 	#[NoAdminRequired]
 	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
 	#[FrontpageRoute(verb: 'GET', url: '/')]
 	public function index(): TemplateResponse {
-		try {
-			$configurationLink = ServerConfigurationUtils::getServerConfigurationLink($this->appConfig, $this->oidcClientMapper, $this->urlGenerator);
-		} catch (Exception $e) {
-			$this->logger->error(get_class($this) . ": cannot generate configuration link: " . $e);
-			// TODO handle error (show error in window ?)
-			$configurationLink = "";
-		}
 		return new TemplateResponse(
 			Application::APP_ID,
 			'index',
-			['configurationLink' => $configurationLink]
 		);
 	}
 }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,64 @@
 <template>
 	<NcAppContent>
 		<div id="olvid">
-			<h1>Hello world!</h1>
+			<NcButton v-if="!magicLink" :disabled="loading" @click="fetchMagicLink" >
+				Get my Olvid magic link
+			</NcButton>
+
+			<p v-if="error" style="color: red;">{{ error }}</p>
+
+			<NcButton v-if="magicLink" @click="openMagicLink">
+				See magic link
+			</NcButton>
+
+			<NcButton v-if="magicLink" @click="openMagicLinkWithOlvid">
+				Open magic link with Olvid
+			</NcButton>
 		</div>
 	</NcAppContent>
 </template>
 
 <script>
+import axios from '@nextcloud/axios'
+import { generateOcsUrl } from '@nextcloud/router'
 import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
+import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 
 export default {
 	name: 'App',
-	components: {
-		NcAppContent,
+	components: { NcAppContent, NcButton },
+	data() {
+		return {
+			loading: false,
+			magicLink: null,
+			error: null,
+		}
+	},
+	methods: {
+		async fetchMagicLink() {
+			this.loading = true
+			this.error = null
+			this.magicLink = null
+			try {
+				const url = generateOcsUrl('/apps/olvid/olvid-rest/magicLink')
+				const response = await axios.get(url)
+				this.magicLink = response.data.configurationUrl
+			} catch (e) {
+				this.error = 'Could not generate magic link: ' + (e.response?.data?.error ?? e.message)
+			} finally {
+				this.loading = false
+			}
+		},
+		async openMagicLink() {
+			if (this.magicLink) {
+				window.open(this.magicLink, '_blank');
+			}
+		},
+		async openMagicLinkWithOlvid() {
+			if (this.magicLink) {
+				window.open(this.magicLink.replace('http://', 'olvid://').replace('https://', 'olvid://'));
+			}
+		},
 	},
 }
 </script>
