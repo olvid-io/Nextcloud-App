f3f40a1587e9c405ddcc43428ed70d1a2774a7f1
jmartel <jeremie.martel@olvid.io>
2026-08-18 11:49:29 +0200

    RandomUtil.php: changed uuid size

--- lib/Utils/RandomUtil.php
+++ lib/Utils/RandomUtil.php
@@ -6,8 +6,7 @@ use Exception;
 
 class RandomUtil {
 	public static function uuid_create(): string {
-		// TODO change to 32u
-		$bytes = RandomUtil::random_bytes(16);
+		$bytes = RandomUtil::random_bytes(32);
 		$bytes[6] = chr(ord($bytes[6]) & 0x0f | 0x40); // version 4
 		$bytes[8] = chr(ord($bytes[8]) & 0x3f | 0x80); // variant RFC 4122
 		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($bytes), 4));
